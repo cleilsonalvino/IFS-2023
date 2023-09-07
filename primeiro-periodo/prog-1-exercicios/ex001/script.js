77f90ddfe810ddefc253d399be8cb461820ad30a
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2023-09-07 20:36:48 -0300

    atividade terminada

--- primeiro-periodo/prog-1-exercicios/ex001/script.js
+++ primeiro-periodo/prog-1-exercicios/ex001/script.js
@@ -22,3 +22,70 @@ function verificar() {
     
 
 }
+
+function verificar2() {
+    var numero = document.getElementById('num')
+    var num = Number(numero.value)
+    var res = document.getElementById('res-1')
+
+    if (numero.value.length == 0) {
+        alert('Digite um valor!')
+    } else if (num % 2 == 0) {
+        res.innerHTML = 'Deu par!'
+    } else {
+        res.innerHTML = 'Deu ímpar!'
+    }
+}
+
+function verificar3() {
+    var numero1 = document.getElementById('nump')
+    var numero2 = document.getElementById('nums')
+    var num1 = Number(numero1.value)
+    var num2 = Number(numero2.value)
+    var res = document.getElementById('res-2')
+
+    if (numero1.value.length == 0 || numero2.value.length == 0) {
+        alert('Preencha todos os campos!')
+    } else if (num1 > num2) {
+        res.innerHTML = `${num1} é o maior!`
+    } else {
+        res.innerHTML = `${num2} é o maior!`
+    }
+}
+
+
+function verificar4() {
+    var numero = document.getElementById('numpn')
+    var num = Number(numero.value)
+    var res = document.getElementById('res-3')
+
+    if (numero.value.length == 0) {
+        alert('Valor inválido!')
+    } else if(num < 0){
+        res.innerHTML = 'Valor negativo!'
+    } else if (num === 0) {
+        res.innerHTML = 'Zero é neutro!'
+    } else {
+        res.innerHTML = 'Valor positivo!'
+    }
+}
+
+function verificar5() {
+    var numero1 = document.getElementById('num-1')
+    var numero2 = document.getElementById('num-2')
+    var numero3 = document.getElementById('num-3')
+    var num1 = Number(numero1.value)
+    var num2 = Number(numero2.value)
+    var num3 = Number(numero3.value)
+    var res = document.getElementById('res-4')
+
+    if (numero1.value.length == 0 || numero2.value.length == 0 || numero3.value.length == 0) {
+        alert('Preencha todos os campos!')
+    } else if (num1 > num2) {
+        res.innerHTML = `${num1} é o maior!`
+    } else if (num2 > num1 && num3 < num2) {
+        res.innerHTML = `${num2} é o maior!`
+    } else {
+        res.innerHTML = `${num3} é o maior!`
+    }
+}
